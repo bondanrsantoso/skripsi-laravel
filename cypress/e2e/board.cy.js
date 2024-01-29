d0a05d1f8e799961ea09eee0af4cbd8965247c76
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2024-01-30 01:22:19 +0700

    Fix cypress test specs

--- cypress/e2e/board.cy.js
+++ cypress/e2e/board.cy.js
@@ -1,4 +1,4 @@
-import { fakerID_ID as faker } from "@faker-js/faker";
+import { fakerID_ID as faker, fakerEN_US } from "@faker-js/faker";
 
 describe(
     "Board list testing",
@@ -6,8 +6,11 @@ describe(
         testIsolation: false,
     },
     () => {
+        before(() => {
+            cy.exec("php artisan migrate:fresh --step --seed");
+        });
         beforeEach(() => {
-            cy.login("rmayasari@example.com", "password");
+            cy.login("test_user@example.com", "password");
         });
 
         it("Opens board list", () => {
@@ -17,29 +20,102 @@ describe(
         });
 
         it("Add new board and sets its title", () => {
-            // cy.login("foo@example.com", "password");
+            interceptSync();
+
             cy.visit("/dashboard/boards");
             cy.contains("Tambah baru").click();
             cy.get("#board-title").clear();
+
             for (let i = 0; i < 10; i++) {
                 cy.get("#board-title").clear().type(faker.company.buzzPhrase());
-                cy.wait(666); //ms
+                cy.wait("@boardUpdate")
+                    .its("response.statusCode")
+                    .should("be.within", 200, 399);
             }
+            cy.get("#board-title").clear().type(faker.string.alphanumeric(256));
+            cy.wait("@boardUpdate")
+                .its("response.statusCode")
+                .should("be.within", 200, 399);
+
             cy.get("#board-title").clear().type("TEST-BOARD");
 
-            cy.wait(1500); // wait for all update sync to finish
+            cy.wait("@boardUpdate")
+                .its("response.statusCode")
+                .should("be.within", 200, 399);
+        });
+
+        it("Clear out board's title", () => {
+            interceptSync();
+            cy.visit("/dashboard/boards");
+            cy.contains("TEST-BOARD").click();
+
+            // const randomTitle = "UPDATED-BOARD-" + faker.string.alpha(5);
+            cy.get("#board-title").clear();
+
+            cy.contains("Tanpa Judul").should("exist");
+            cy.get("#board-title").clear().type("TEST-BOARD");
+            cy.wait("@boardUpdate")
+                .its("response.statusCode")
+                .should("be.within", 200, 399);
         });
 
         it("update existing board", () => {
+            interceptSync();
             cy.visit("/dashboard/boards");
             cy.contains("TEST-BOARD").click();
 
             const randomTitle = "UPDATED-BOARD-" + faker.string.alpha(5);
             cy.get("#board-title").clear().type(randomTitle);
 
-            cy.wait(1500);
+            cy.wait("@boardUpdate")
+                .its("response.statusCode")
+                .should("be.within", 200, 399);
+
             cy.visit("/dashboard/boards");
             cy.contains(randomTitle).click();
         });
+
+        it("Fill out notes", () => {
+            // interceptSync();
+            interceptNewNoteRequest();
+            interceptNoteUpdate();
+
+            cy.visit("/dashboard/boards");
+            cy.contains("UPDATED-BOARD").last().click();
+
+            for (let i = 0; i < 3; i++) {
+                cy.contains("Tambah Teks").click();
+                cy.wait("@newNoteRequest")
+                    .its("response.statusCode")
+                    .should("be.within", 200, 399);
+            }
+
+            cy.get(".ql-editor").each(($node, i, $nodes) => {
+                cy.wrap($node).focus().type(faker.word.words(100));
+                cy.wait("@noteUpdateRequest")
+                    .its("response.statusCode")
+                    .should("be.within", 200, 399);
+            });
+        });
     }
 );
+
+function interceptNoteUpdate() {
+    cy.intercept({
+        url: "/dashboard/boards/*/board_notes/*",
+        method: "PUT",
+    }).as("noteUpdateRequest");
+}
+
+function interceptNewNoteRequest() {
+    cy.intercept({ url: "/dashboard/boards/*/board_notes", method: "POST" }).as(
+        "newNoteRequest"
+    );
+}
+
+function interceptSync() {
+    cy.intercept({
+        method: "PUT",
+        url: "/dashboard/boards/*",
+    }).as("boardUpdate");
+}
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,120 @@
 describe("Login Page Testing", () => {
+    before(() => {
+        cy.exec("php artisan migrate:fresh --step --seed");
+    });
+
     it("Opens login page", () => {
         cy.visit("/login");
     });
 
-    it("Fills out the login form", () => {
+    function interceptRedirect() {
+        cy.intercept({
+            method: "GET",
+            url: "/dashboard/boards",
+        }).as("loginRequestCheck");
+    }
+
+    function interceptLogin() {
+        cy.intercept({
+            method: "POST",
+            url: "/login",
+        }).as("loginRequest");
+    }
+
+    it("Test empty form", () => {
+        interceptLogin();
         cy.visit("/login");
-        cy.get("input[type=email]").type("raina34@example.com");
+        cy.get("button").contains("Log in").click();
+
+        // It doesn't redirect
+        cy.wait("@loginRequest");
+        cy.location().its("pathname").should("equal", "/login");
+    });
+
+    it("Leaves out password", () => {
+        interceptLogin();
+        cy.visit("/login");
+        cy.get("input[type=email]").type("test_user@example.com");
+        cy.get("button").contains("Log in").click();
+
+        // It doesn't redirect
+        cy.wait("@loginRequest");
+        cy.location().its("pathname").should("equal", "/login");
+    });
+
+    it("Leaves out email", () => {
+        interceptLogin();
+        cy.visit("/login");
         cy.get("input[type=password]").type("password");
         cy.get("button").contains("Log in").click();
+
+        // It doesn't redirect
+        cy.wait("@loginRequest");
+        cy.location().its("pathname").should("equal", "/login");
+    });
+
+    it("Test wrong email", () => {
+        interceptLogin();
+        cy.visit("/login");
+        cy.get("input[type=email]").type("not_test_user@example.com");
+        cy.get("input[type=password]").type("password");
+        cy.get("button").contains("Log in").click();
+
+        // It doesn't redirect
+        cy.wait("@loginRequest");
+        cy.location().its("pathname").should("equal", "/login");
+    });
+
+    it("Test wrong password", () => {
+        interceptLogin();
+        cy.visit("/login");
+        cy.get("input[type=email]").type("test_user@example.com");
+        cy.get("input[type=password]").type("wrongpassword");
+        cy.get("button").contains("Log in").click();
+
+        // It doesn't redirect
+        cy.wait("@loginRequest");
+        cy.location().its("pathname").should("equal", "/login");
+    });
+
+    it("Test wrong email and password", () => {
+        interceptLogin();
+        cy.visit("/login");
+        cy.get("input[type=email]").type("not_test_user@example.com");
+        cy.get("input[type=password]").type("wrongpassword");
+        cy.get("button").contains("Log in").click();
+
+        // It doesn't redirect
+        cy.wait("@loginRequest");
+        cy.location().its("pathname").should("equal", "/login");
+    });
+
+    it("Login with correct email and password", () => {
+        interceptRedirect();
+        cy.visit("/login");
+        cy.get("input[type=email]").type("test_user@example.com");
+        cy.get("input[type=password]").type("password");
+        cy.get("button").contains("Log in").click();
+
+        // cy.visit("/dashboard/boards");
+        // cy.contains("Tambah baru").should("not.exist");
+        cy.wait("@loginRequestCheck")
+            .its("response.statusCode")
+            .should("equal", 200);
+    });
+
+    it("Login with correct email and password and remember me", () => {
+        interceptRedirect();
+        cy.visit("/login");
+        cy.get("input[type=email]").type("test_user@example.com");
+        cy.get("input[type=password]").type("password");
+        cy.get("input[type=checkbox]").check();
+        cy.get("button").contains("Log in").click();
+
+        // cy.visit("/dashboard/boards");
+        // cy.contains("Tambah baru").should("not.exist");
+        cy.wait("@loginRequestCheck")
+            .its("response.statusCode")
+            .should("equal", 200);
     });
 });
